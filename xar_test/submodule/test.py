3972b0593d07a2e32bb2c2cd4b81717c0369a49e
Nick Terrell <terrelln@fb.com>
2018-08-06 10:16:34 -0700

    Add relative imports

--- xar_test/submodule/test.py
+++ xar_test/submodule/test.py
@@ -1,6 +1,7 @@
 from xar_test.bar import bar
-
+from .foo import foo
 
 def main():
+    foo()
     bar()
     print("xar_test.submodule.test:main")
--- xar_test/test.py
+++ xar_test/test.py
@@ -1,5 +1,7 @@
 from xar_test.submodule.foo import foo
+from .bar import bar
 
 def main():
     foo()
+    bar()
     print("xar_test.test:main")
